aa5e3ad9efa1a4646cf33333a0b221bd13fa3bf4
Test <nikhil.jain0987@gmail.com>
2015-09-30 00:07:03 -0400

    GOT TUTORIAL WORKING-DETECTS IF USER IS IN THE AREA OR NOT

---
+++ EstimoteFun/app/src/main/java/com/nikhil/estimotefun/MyApplication.java
@@ -0,0 +1,81 @@
+package com.nikhil.estimotefun;
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+
+import java.util.List;
+import java.util.UUID;
+
+/*
+*
+* To set up the beacon managor (not bacon!)
+*
+* */
+public class MyApplication extends Application {
+
+    private BeaconManager beaconManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        beaconManager = new BeaconManager(getApplicationContext());
+
+        beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
+            @Override
+            public void onEnteredRegion(Region region, List<Beacon> beacons) {
+                showNotification("You entered the region", "Thanks for letting us know!");
+            }
+
+            @Override
+            public void onExitedRegion(Region region) {
+                showNotification("You left", "Thanks for letting us know.");
+            }
+        });
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startMonitoring(new Region("monitored region",
+                        UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), 26276, 60209));
+            }
+        });
+
+    } // end of oncreate
+
+
+    // helper method
+    public void showNotification(String title, String message) {
+
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0, new Intent[] {notifyIntent}, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        notification.defaults |= Notification.DEFAULT_SOUND;
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(1, notification);
+
+    }
+
+
+}
